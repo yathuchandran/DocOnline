846fe0dbb32d16238a963d4d284c40e25cfc47e4
yathuchandran <yathuchandran67437@gmail.com>
2023-08-21 18:57:37 +0530

    connected database

--- server/index.js
+++ server/index.js
@@ -1,19 +1,37 @@
+//import userRoute from './routes/userRoute.js'
+ const mongoose = require('mongoose')
 const express = require("express");
 const app = express();
+const userRoute = require('./routes/userRoute')
+
+
 
 const path = require("path");
 
 
-const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://yatheeshbc8:sanidhya*88@cluster0.sjvvmgl.mongodb.net/ONLINEDOC");
+mongoose.connect("mongodb+srv://yatheeshbc8:tDRpoZqglS7GfVVp@cluster0.sjvvmgl.mongodb.net/?retryWrites=true&w=majority");
+
 mongoose.set("strictQuery", false);
 
-//mongoose.connect("mongodb+srv://yatheeshbc8:sanidhya*88@cluster0.sjvvmgl.mongodb.net/?retryWrites=true&w=majority");
 
 
+// app.use(morgan("dev"))
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static(path.join(__dirname, "public"))); //Register your public folder to express in your .js file by
 
+const createError = require('http-errors')
+
+
+//view engine setup
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
+app.use(function(req, res, next) { 
+    res.header('Cache-Control', 'no-cache, no-store');
+    next();
+});
  
 //register partial route setup
 app.use(express.static(__dirname + '/public'));
@@ -21,6 +39,10 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
+
+
+app.use('/', userRoute)
 
 
 
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,3 +1,6 @@
 
 const express = require ("express") 
 const user_route = express.Router();
+
+
+module.exports = user_route;
