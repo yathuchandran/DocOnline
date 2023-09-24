1c97e6892a8f8899e88a23465ca9043c6adf5c66
yathuchandran <yathuchandran67437@gmail.com>
2023-09-24 12:05:15 +0530

    reset pass

--- client/src/components/ResetPassword.jsx
+++ client/src/components/ResetPassword.jsx
@@ -2,7 +2,71 @@ import React from 'react'
 
 function ResetPassword() {
   return (
-    <div>ResetPassword</div>
+    <section className="otpForm">
+    <div
+      className="otp-container text-center d-flex  align-items-center justify-content-center min-vh-100 "
+      style={{
+        background: "linear-gradient(to bottom, rgb(190, 181, 199), #002147)",
+      }}
+    >
+      <div
+        className="signu-form bg-white row d-flex justify-content-center align-items-center h-100 col-md-4"
+        style={{
+          border: "1px solid rgb(219, 217, 217)",
+          borderRadius: "15px",
+        }}
+      >
+        <form action="">
+          <h1 style={{ fontFamily: "Times New Roman, serif" }}>ENTER-OTP</h1>
+
+          <label htmlFor="otp">Enter otp</label>
+          {/* {errorMsg ? (
+            <div className=" mt-1 alert alert-danger text-10" role="alert">
+              {errorMsg}
+            </div>
+          ) : (
+            ""
+          )} */}
+
+          <div className=" text-center    ">
+            <div class="position-relative">
+              <div class="   ">
+                <h6>
+                  Please enter the one-time password <br /> to verify your
+                  account
+                </h6>
+                <div>
+                  <span>A code has been sent to</span>{" "}
+                  <small>*******@gmail.com</small>
+                </div>
+                <input
+                  type="number"
+                  max={9999}
+                  min={1000}
+                 
+                  className="form-control "
+                />
+
+                <div class="mt-4">
+                  <button
+                    type="button"
+                    className="btn btn-success btn-lg btn-md"
+                    style={{
+                      backgroundColor: "#002147", // Add the background color here
+                      color: "white", // Optionally, set the text color
+                    }}
+                  >
+                    Validate
+                  </button>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </section>
   )
 }
 
